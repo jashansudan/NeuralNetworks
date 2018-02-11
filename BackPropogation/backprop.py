e641fde665aa9594e36e49daab63dc04afb8e4e3
jashansudan <jashansudan@gmail.com>
2018-02-11 15:19:15 -0500

    removed unused perceptron files

--- BackPropogation/backprop.py
+++ BackPropogation/backprop.py
@@ -2,7 +2,7 @@ from random import random
 from csv import reader
 
 
-def get_data(filename):
+def getData(filename):
     data = []
     with open(filename, 'r') as file:
         csv_reader = reader(file)
@@ -10,7 +10,35 @@ def get_data(filename):
             if not row:
                 continue
             data.append(row)
+    data = data[1:]
     return data
 
 
-print get_data("assignment2data.csv")
+def convertToFloat(data):
+    for row in range(len(data)):
+        for col in range(len(data[0])):
+            data[row][col] = float(data[row][col])
+    return
+
+
+def normalizeData(data):
+    for col in range(len(data[0])):
+        col_max = float("-inf")
+        col_min = float("inf")
+        for row in range(len(data)):
+            if data[row][col] > col_max:
+                col_max = data[row][col]
+            if data[row][col] < col_min:
+                col_min = data[row][col]
+        print col_max, col_min
+
+        for row in range(len(data)):
+            data[row][col] = (data[row][col] - col_min) / (col_max - col_min)
+    return
+
+
+data = getData("assignment2data.csv")
+convertToFloat(data)
+normalizeData(data)
+for i in data:
+    print i
